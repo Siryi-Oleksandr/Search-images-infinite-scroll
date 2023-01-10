789be6d900ceeb95e339887def69d67f95961b04
Siryi-Oleksandr <siryyoleksandr@gmail.com>
2023-01-10 11:34:10 +0200

    initial settings2

--- src/js/refs.js
+++
@@ -1,7 +0,0 @@
-export const refs = {
-  searchForm: document.querySelector('#search-form'),
-  btnSearch: document.querySelector('[type="submit"]'),
-  galleryContainer: document.querySelector('.gallery'),
-  btnUp: document.querySelector('#back-top'),
-  loader: document.querySelector('#loading'),
-};
